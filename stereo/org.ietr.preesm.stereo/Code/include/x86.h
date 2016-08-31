51bf0ae3312591589301c105533e919d0c5e4b19
Pelcat Maxime <mpelcat@insa-rennes.fr>
2016-08-31 17:16:33 +0200

    (Stereo) Ensuring that the POSIX version is 2001, supporting barriers under Linux.

--- stereo/org.ietr.preesm.stereo/Code/include/x86.h
+++ stereo/org.ietr.preesm.stereo/Code/include/x86.h
@@ -11,6 +11,21 @@
 #ifndef X86_H
 #define X86_H
 
+
+// For Linux
+// Pthread barriers are defined in POSIX 2001 version
+// For the 1990 revision compliance the defined value of _POSIX_VERSION should be 1.
+// For the 1995 revision compliance the defined value of _POSIX_VERSION should be 199506L.
+// For the 2001 revision compliance the defined value of _POSIX_VERSION should be 200112L.
+#ifndef _POSIX_C_SOURCE
+#define _POSIX_C_SOURCE 200112L
+#endif
+
+#ifndef _XOPEN_SOURCE
+#define _XOPEN_SOURCE 600
+#endif
+
+
 #include <string.h>
 #include <pthread.h>
 #include <semaphore.h>
