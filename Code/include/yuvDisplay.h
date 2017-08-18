9f836e33d325a4f61c3c6d8392612e237b38fc4a
kdesnos <kdesnos@insa-rennes.fr>
2017-08-18 14:45:07 +0200

    (Stabilization) Display with FPS.

--- Code/include/yuvDisplay.h
+++ Code/include/yuvDisplay.h
@@ -1,18 +1,17 @@
 /*
-	============================================================================
-	Name        : displayYUV.h
-	Author      : mpelcat & kdesnos
-	Version     :
-	Copyright   :
-	Description : Displaying YUV frames one next to another in a row.
-	============================================================================
-	*/
+============================================================================
+Name        : displayYUV.h
+Author      : mpelcat & kdesnos & jheulot
+Version     :
+Copyright   :
+Description : Displaying YUV frames one next to another in a row.
+============================================================================
+*/
+#ifndef DISPLAY_YUV
+#define DISPLAY_YUV
 
 #include "preesm.h"
 #include "applicationParameters.h"
-
-#ifndef DISPLAY_YUV
-#define DISPLAY_YUV
 
 #define NB_DISPLAY 1
 #define DISPLAY_W (2*BORDER+WIDTH)*NB_DISPLAY
@@ -20,10 +19,13 @@
 
 #define INIT_OVERLAY {NULL} // must be se same size as NB_DISPLAY
 
+#define PATH_TTF PROJECT_ROOT_PATH "/dat/arial.ttf"
+
 /**
 * Function called to display one of the YUV frame of the window.
 * The size of the displayed frame must correspond to the one declared
 * when initializing the display.
+* This function also display the number of Frames per seconds.
 *
 * @param id
 *        The id of the frame to display
@@ -35,6 +37,26 @@
 *        the V component of the frame to display
 */
 void yuvDisplay(int id, IN unsigned char *y, IN unsigned char *u, IN unsigned char *v);
+
+/**
+* Function called to display one of the YUV frame of the window.
+* The size of the displayed frame must correspond to the one declared
+* when initializing the display.
+* This function also display the number of Frames per seconds and the number 
+* of slices used for parallelization.
+*
+* @param id
+*        The id of the frame to display
+* @param nbSlice
+*        The number of slice used to parallelize computations.
+* @param y
+*        the Y component of the frame to display
+* @param u
+*        the U component of the frame to display
+* @param v
+*        the V component of the frame to display
+*/
+void yuvDisplayWithNbSlice(int id, int nbSlice, IN unsigned char *y, IN unsigned char *u, IN unsigned char *v);
 
 /**
 * Initialize a new display with a parameterizable resolution and open a
--- Code/src/yuvDisplay.c
+++ Code/src/yuvDisplay.c
@@ -1,19 +1,22 @@
 /*
-	============================================================================
-	Name        : displayYUV.c
-	Author      : mpelcat & kdesnos
-	Version     :
-	Copyright   : CECILL-C
-	Description : Displaying YUV frames one next to another in a row
-	============================================================================
-	*/
+============================================================================
+Name        : displayYUV.c
+Author      : mpelcat & kdesnos & jheulot
+Version     :
+Copyright   : CECILL-C
+Description : Displaying YUV frames one next to another in a row
+============================================================================
+*/
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
 #include "yuvDisplay.h"
+#include "clock.h"
 #include <SDL.h>
+#include <SDL_ttf.h>
 
+#define FPS_MEAN 5
 
 extern int stopThreads;
 
@@ -25,14 +28,25 @@ typedef struct YuvDisplay
 	SDL_Texture* textures[NB_DISPLAY];	    // One overlay per frame
 	SDL_Window *screen;					    // SDL surface where to display
 	SDL_Renderer *renderer;
+	TTF_Font *text_font;
 	int currentXMin;						// Position for next display
 	int initialized;                        // Initialization done ?
-
+	int stampId;
 } YuvDisplay;
 
 
 // Initialize
-YuvDisplay display = { .textures = { NULL }, .initialized = 0 };
+static YuvDisplay display;
+
+int exitCallBack(void* userdata, SDL_Event* event){
+	if (event->type == SDL_QUIT){
+		printf("Exit request from GUI.\n");
+		stopThreads = 1;
+		return 0;
+	}
+
+	return 1;
+}
 
 /**
 * Initializes a display frame. Be careful, once a window size has been chosen,
@@ -80,14 +94,34 @@ void yuvDisplayInit(int id, int width, int height)
 		char* name = "Display";
 		display.initialized = 1;
 
+		printf("SDL_Init_Start\n");
+
 		if (SDL_Init(SDL_INIT_VIDEO))
 		{
 			fprintf(stderr, "Could not initialize SDL - %s\n", SDL_GetError());
 			exit(1);
 		}
 
+		printf("SDL_Init_end\n");
+
+		/* Initialize SDL TTF for text display */
+		if (TTF_Init())
+		{
+			printf("TTF initialization failed: %s\n", TTF_GetError());
+		}
+
+		printf("TTF_Init\n");
+
+		/* Initialize Font for text display */
+		//display.text_font = TTF_OpenFont("./arial.ttf", 20);
+		display.text_font = TTF_OpenFont(PATH_TTF, 20);
+		if (!display.text_font)
+		{
+			printf("TTF_OpenFont: %s\n", TTF_GetError());
+		}
+
 		display.screen = SDL_CreateWindow(name, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-										  DISPLAY_W, DISPLAY_H, SDL_WINDOW_SHOWN);
+			DISPLAY_W, DISPLAY_H, SDL_WINDOW_SHOWN);
 		if (!display.screen)
 		{
 			fprintf(stderr, "SDL: could not set video mode - exiting\n");
@@ -106,9 +140,9 @@ void yuvDisplayInit(int id, int width, int height)
 	{
 
 		display.textures[id] = SDL_CreateTexture(display.renderer,
-												 SDL_PIXELFORMAT_IYUV,
-												 SDL_TEXTUREACCESS_STREAMING,
-												 width, height);
+			SDL_PIXELFORMAT_IYUV,
+			SDL_TEXTUREACCESS_STREAMING,
+			width, height);
 
 		if (!display.textures[id])
 		{
@@ -119,12 +153,21 @@ void yuvDisplayInit(int id, int width, int height)
 		display.currentXMin += width;
 	}
 
+	display.stampId = 0;
+	for (int i = 0; i<FPS_MEAN; i++){
+		startTiming(i + 1);
+	}
+
+	printf("register\n");
+	SDL_SetEventFilter(exitCallBack, NULL);
 }
 
+void yuvDisplay(int id, unsigned char *y, unsigned char *u, unsigned char *v){
+	yuvDisplayWithNbSlice(id, -1, y, u, v);
+}
 
-void yuvDisplay(int id, unsigned char *y, unsigned char *u, unsigned char *v)
+void yuvDisplayWithNbSlice(int id, int nbSlice, unsigned char *y, unsigned char *u, unsigned char *v)
 {
-
 	SDL_Texture* texture = display.textures[id];
 	int w, h;
 
@@ -138,32 +181,73 @@ void yuvDisplay(int id, unsigned char *y, unsigned char *u, unsigned char *v)
 		v, w / 2
 		);
 
-	yuvRefreshDisplay(id);
-}
-
-
-void yuvRefreshDisplay(int id)
-{
-	SDL_Texture* texture = display.textures[id];
-	SDL_Event event;
 	SDL_Rect screen_rect;
 
-	SDL_QueryTexture(texture, NULL, NULL, &(screen_rect.w), &(screen_rect.h));
-
-	screen_rect.x = screen_rect.w*id;
+	screen_rect.w = w;
+	screen_rect.h = h;
+	screen_rect.x = w*id;
 	screen_rect.y = 0;
 
 	SDL_RenderCopy(display.renderer, texture, NULL, &screen_rect);
+
+	/* Draw FPS text */
+	char fps_text[20];
+	SDL_Color colorWhite = { 255, 255, 255, 255 };
+
+	int time = stopTiming(display.stampId + 1);
+	sprintf(fps_text, "FPS: %.2f", 1. / (time / 1000000. / FPS_MEAN));
+	startTiming(display.stampId + 1);
+	display.stampId = (display.stampId + 1) % FPS_MEAN;
+
+	SDL_Surface* fpsText = TTF_RenderText_Blended(display.text_font, fps_text, colorWhite);
+	SDL_Texture* fpsTexture = SDL_CreateTextureFromSurface(display.renderer, fpsText);
+
+	int fpsWidth, fpsHeight;
+	SDL_QueryTexture(fpsTexture, NULL, NULL, &fpsWidth, &fpsHeight);
+	SDL_Rect fpsTextRect;
+
+	fpsTextRect.x = 0;
+	fpsTextRect.y = 0;
+	fpsTextRect.w = fpsWidth;
+	fpsTextRect.h = fpsHeight;
+	SDL_RenderCopy(display.renderer, fpsTexture, NULL, &fpsTextRect);
+
+	/* Free resources */
+	SDL_FreeSurface(fpsText);
+	SDL_DestroyTexture(fpsTexture);
+
+	/* Draw NbSlice Text */
+	if (nbSlice > 0){
+		char slice_text[20];
+
+		sprintf(slice_text, "nbSlice: %d", nbSlice);
+
+		SDL_Surface* sliceText = TTF_RenderText_Blended(display.text_font, slice_text, colorWhite);
+		SDL_Texture* sliceTexture = SDL_CreateTextureFromSurface(display.renderer, sliceText);
+
+		int sliceWidth, sliceHeight;
+		SDL_QueryTexture(sliceTexture, NULL, NULL, &sliceWidth, &sliceHeight);
+		SDL_Rect sliceTextRect;
+
+		sliceTextRect.x = 0;
+		sliceTextRect.y = fpsHeight;
+		sliceTextRect.w = sliceWidth;
+		sliceTextRect.h = sliceHeight;
+		SDL_RenderCopy(display.renderer, sliceTexture, NULL, &sliceTextRect);
+
+		/* Free resources */
+		SDL_FreeSurface(sliceText);
+		SDL_DestroyTexture(sliceTexture);
+	}
+
 	SDL_RenderPresent(display.renderer);
 
+	SDL_Event event;
 	// Grab all the events off the queue.
 	while (SDL_PollEvent(&event))
 	{
 		switch (event.type)
 		{
-		case SDL_QUIT:
-			stopThreads = 1;
-			break;
 		default:
 			break;
 		}
@@ -177,4 +261,3 @@ void yuvFinalize(int id)
 	SDL_DestroyRenderer(display.renderer);
 	SDL_DestroyWindow(display.screen);
 }
-
