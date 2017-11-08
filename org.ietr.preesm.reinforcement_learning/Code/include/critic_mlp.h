22df87f5054e736c01350dd36989e4be02f138bc
Florian Arrestier <farresti@insa-rennes.fr>
2017-11-08 08:57:06 +0100

    updated training graph

--- org.ietr.preesm.reinforcement_learning/Code/include/critic_mlp.h
+++ org.ietr.preesm.reinforcement_learning/Code/include/critic_mlp.h
@@ -9,4 +9,7 @@
 
 void criticWeightGenInit(int id, OUT float *weights_out, OUT float *bias_out);
 
+void td_error(IN float gamma,IN float reward, IN float value_state, IN float value_next_state,
+              OUT float *target, OUT float *sigma);
+
 #endif //CRITIC_MLP_H
--- org.ietr.preesm.reinforcement_learning/Code/src/critic_mlp.c
+++ org.ietr.preesm.reinforcement_learning/Code/src/critic_mlp.c
@@ -3,3 +3,10 @@
 //
 
 #include "../include/critic_mlp.h"
+
+
+void td_error(IN float gamma, IN float reward, IN float value_state, IN float value_next_state,
+              OUT float *target, OUT float *sigma) {
+    (*target) = reward + gamma * value_next_state;
+    (*sigma) = (*target) - value_state;
+}
