215db244120db6f0a861e61dabb871e7b068649a
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2018-02-06 09:04:45 +0100

    (AdaptiveRT) Delete unused files

--- org.preesm.poc.adaptativeruntime/ActorSplitCpp/actors.h
+++
@@ -1,9 +0,0 @@
-#include "adaptive_runtime.h"
-
-void initDisplays();
-
-void initRead();
-
-void read(OUT rgbimg* img);
-
-void display(IN rgbimg * img, unsigned int displayID);
--- org.preesm.poc.adaptativeruntime/ActorSplitCpp/adaptive_runtime.h
+++
@@ -1,5 +0,0 @@
-#include "preesm.h"
-#include "image.h"
-
-#define XSAMPLING 800
-#define YSAMPLING 600
