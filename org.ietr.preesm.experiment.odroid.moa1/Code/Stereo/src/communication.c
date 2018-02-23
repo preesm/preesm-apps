72a771135710011ff162b407e88cc8c0c9ecce5b
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2018-02-23 16:00:00 +0100

    (MoA) Fix C files for Stereo

--- org.ietr.preesm.experiment.odroid.moa1/Code/Stereo/src/communication.c
+++ org.ietr.preesm.experiment.odroid.moa1/Code/Stereo/src/communication.c
@@ -2,24 +2,41 @@
 	============================================================================
 	Name        : communication.c
 	Author      : kdesnos
-	Version     : 1.0
+	Version     : 2.1
 	Copyright   : CECILL-C
-	Description : 
+	Description :
 	============================================================================
 */
 
+#include "communication.h"
 
-#include "../include/communication.h"
-#include <stdio.h>
+// 8 local semaphore for each core (1 useless per core)
+sem_t interCoreSem[MAX_NB_CORES][MAX_NB_CORES];
 
-void sendStart(sem_t* sem){
-    sem_post(sem);
+void communicationInit(){
+	int i, j;
+	for (i = 0; i < MAX_NB_CORES; i++){
+		for (j = 0; j < MAX_NB_CORES; j++){
+			sem_init(&interCoreSem[i][j], 0, 0);
+		}
+	}
+}
+
+void sendStart(int senderID, int receiverID){
+#ifndef NDEBUG
+	if(senderID >= MAX_NB_CORES || receiverID >= MAX_NB_CORES){
+		printf("Number of core exceeds the limit specified in communication.h. \n Update MAX_NB_CORES.");
+		exit(-1);
+	}
+
+#endif
+	sem_post(&interCoreSem[receiverID][senderID]);
 }
 
 void sendEnd(){}
 
 void receiveStart(){}
 
-void receiveEnd(sem_t* sem){
-    sem_wait(sem);
+void receiveEnd(int senderID, int receiverID){
+	sem_wait(&interCoreSem[receiverID][senderID]);
 }
