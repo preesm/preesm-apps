17069b57bd683a37e5dfdd3bb8be38686004b4ea
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2018-02-23 15:31:17 +0100

    (MoA) Fix communication lib to match new code generation

--- org.ietr.preesm.experiment.odroid.moa1/Code/include/communication.h
+++ org.ietr.preesm.experiment.odroid.moa1/Code/include/communication.h
@@ -2,7 +2,7 @@
 	============================================================================
 	Name        : communication.h
 	Author      : kdesnos
-	Version     : 1.0
+	Version     : 2.1
 	Copyright   : CECILL-C
 	Description : Communication primitive for Preesm Codegen.
                   Currently, primitives were tested only for x86, shared_mem
@@ -13,14 +13,33 @@
 #ifndef COMMUNICATION_H
 #define COMMUNICATION_H
 
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* Maximum number of core supported by the communication library.
+* This number is used to allocate the table of semaphores used for intercore
+* synchronization.
+*/
+#define MAX_NB_CORES 16
+
+/**
+* Initialize the semaphores used for inter-core synchronization.
+*/
+void communicationInit();
+
 /**
 * Non-blocking function called by the sender to signal that a buffer is ready
 * to be sent.
-* 
-* @param sem
-*        the semaphore used to signal that a data is available.
+*
+* @param[in] senderID
+*        the ID of the sender core
+* @param[in] coreID
+*        the ID of the receiver core
 */
-void sendStart(sem_t* sem);
+void sendStart(int senderID, int receveirID);
 
 /**
 * Blocking function (not for shared_mem communication) called by the sender to
@@ -35,12 +54,14 @@ void sendEnd();
 void receiveStart();
 
 /**
-* Blocking function called by the sender to wait for the received data 
+* Blocking function called by the sender to wait for the received data
 * availability.
 *
-* @param sem
-*        the semaphore used to signal that a data is available.
+* @param[in] senderID
+*        the ID of the sender core
+* @param[in] coreID
+*        the ID of the receiver core
 */
-void receiveEnd(sem_t* sem);
+void receiveEnd(int senderID, int receveirID);
 
 #endif
