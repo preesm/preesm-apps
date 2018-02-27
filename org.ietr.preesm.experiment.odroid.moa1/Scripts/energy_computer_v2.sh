48a35d4fa7e68e8b1faddcf272298112e2838b01
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2018-02-27 14:27:11 +0100

    (MoA) energy computer uses time instead of rate

--- org.ietr.preesm.experiment.odroid.moa1/Scripts/energy_computer_v2.sh
+++ org.ietr.preesm.experiment.odroid.moa1/Scripts/energy_computer_v2.sh
@@ -7,13 +7,16 @@
 MEASURE_FOLDER=$1
 
 POWERFILES=$(ls ${MEASURE_FOLDER}/*_W*.csv)
-MEASURE_RATE=$(cat ${MEASURE_FOLDER}/measure_rate | cut -d' ' -f1)
+MEASURE_TIME=$(cat ${MEASURE_FOLDER}/measure_time.csv | cut -d' ' -f1)
 TOTAL_POWER=0
 for FILE in ${POWERFILES}; do
+  TOTAL_MEASURES=0
   for MEASURE in $(cat ${FILE}); do
     TOTAL_POWER=$(echo "$TOTAL_POWER + $MEASURE" | bc -l)
+    TOTAL_MEASURES=$((TOTAL_MEASURES + 1))
   done
 done
+MEASURE_RATE=
 #TOTAL_POWER=$((TOTAL_POWER / MEASURE_RATE))
-TOTAL_POWER=$(echo "$TOTAL_POWER / $MEASURE_RATE" | bc -l)
+TOTAL_POWER=$(echo "$TOTAL_POWER / ($TOTAL_MEASURES / $MEASURE_TIME)" | bc -l)
 echo $TOTAL_POWER
