557fab64e0eb60050b1e58de616294a450a43b2f
Antoine Morvan <antoine.morvan@insa-rennes.fr>
2018-02-27 15:41:30 +0100

    (MoA) Make sure the configure script init the GPU sensors

--- org.ietr.preesm.experiment.odroid.moa1/Scripts/onboard_scripts/configure.sh
+++ org.ietr.preesm.experiment.odroid.moa1/Scripts/onboard_scripts/configure.sh
@@ -13,6 +13,7 @@ SCRIPT_DIR=$(cd `dirname ${0}`/ && pwd)
 echo 1 > ${INA_DRV_BASEPATH}/${INA_DRV_ID}-0045/enable
 echo 1 > ${INA_DRV_BASEPATH}/${INA_DRV_ID}-0040/enable
 echo 1 > ${INA_DRV_BASEPATH}/${INA_DRV_ID}-0041/enable
+echo 1 > ${INA_DRV_BASEPATH}/${INA_DRV_ID}-0044/enable
 
 # settle two seconds to the sensors get fully enabled and have the first reading
 sleep 2
