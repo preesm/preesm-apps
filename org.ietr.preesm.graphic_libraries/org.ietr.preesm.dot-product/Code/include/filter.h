b0c633cae786c3a5780c640525c488c1ebd2dbdb
Zhang Zhijin <zhzhang@insa-rennes.fr>
2018-07-23 10:34:41 +0200

    [Dot product] added comments

--- org.ietr.preesm.graphic_libraries/org.ietr.preesm.dot-product/Code/include/filter.h
+++ org.ietr.preesm.graphic_libraries/org.ietr.preesm.dot-product/Code/include/filter.h
@@ -1,18 +1,37 @@
+/*
+ ============================================================================
+ Name        : filter.h
+ Author      : zhzhang
+ Version     : 1.1
+ Copyright   : CeCILL-C
+ Description : 2D filtering function
+ ============================================================================
+ */
+
 #ifndef FILTER_H
 #define FILTER_H
 
 #include "preesm.h"
 
 /**
- * Function to apply a filter to an image of size width*height.
- * The 1 pixel-wide border of the image will not be computed.
+ * Function to apply a dot-product kernel to an image of size width*height.
+ * The coefficients of the filter are fed to the actor by from
+ * input along with a normalizing constant. A borderSize-width
+ * border will not be computed and will be given the value 0 in
+ * the output
  *
  * @param width
  *        The width of the processed image
  * @param height
- *        The heigth of the processed image
+ *        The height of the processed image
  * @param filterSize
  * 		  The size of the filter
+ * @param borderSize
+ * 		  The border which is not computed
+ * @param coefficients
+ * 		  The coefficients of the filter
+ * @param norm
+ * 		  The normalizing constant
  * @param input
  *        The input image
  * @param output
