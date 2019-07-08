2f59a4b2a0a23e4116333ad3840a0e4eef3e13f5
Morvan Antoine <antoine.morvan@insa-rennes.fr>
2019-07-08 17:32:33 +0200

    (Sobel) Move constants to preesm.h

--- tutorials/org.ietr.preesm.sobel/Code/include/preesm.h
+++ tutorials/org.ietr.preesm.sobel/Code/include/preesm.h
@@ -23,4 +23,22 @@
 
 typedef unsigned char uchar;
 
+#define NB_DISPLAY 1
+
+#define PATH PROJECT_ROOT_PATH "/dat/akiyo_cif.yuv"
+#define NB_FRAME 300
+#define DISPLAY_W 352*NB_DISPLAY
+#define DISPLAY_H 288
+
+/*
+#define PATH PROJECT_ROOT_PATH "/dat/sunflower_1080p25.yuv"
+#define NB_FRAME 500
+#define DISPLAY_W 1920*NB_DISPLAY
+#define DISPLAY_H 1080
+*/
+
+#define INIT_OVERLAY {NULL} // must be se same size as NB_DISPLAY
+
+#define PATH_TTF PROJECT_ROOT_PATH "/dat/DejaVuSans.ttf"
+
 #endif
