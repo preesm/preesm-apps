bda72a1370325f93fb564f5f7ae0556697862e71
Morvan Antoine <antoine.morvan@insa-rennes.fr>
2019-10-14 16:55:11 +0200

    (Scripts) Fix examples to run in Xvfb

--- tutorials/org.ietr.preesm.sobel/preesm_integration_config.sh
+++ tutorials/org.ietr.preesm.sobel/preesm_integration_config.sh
@@ -9,9 +9,9 @@ export REF_SCENARIO=1core_sobel.scenario
 export REF_WORKFLOW=Codegen.workflow
 
 function preesm_build_project () {
-  mkdir -p Code/bin && cd Code/bin && rm -rf * && cmake -D CMAKE_C_FLAGS="-DPREESM_VERBOSE -DPREESM_LOOP_SIZE=500 " .. && make -j4
+  (mkdir -p Code/bin && cd Code/bin && rm -rf * && cmake -D CMAKE_C_FLAGS="-DPREESM_VERBOSE -DPREESM_LOOP_SIZE=500 " .. && make -j4)
 }
 
 function preesm_exec_project () {
-  cd Code/ && ./bin/Release/sobel
+  (cd Code/ && xvfb-run -a --server-args="-ac -screen 0 1280x1024x16" ./bin/Release/sobel)
 }
