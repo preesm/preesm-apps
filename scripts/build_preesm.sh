275f6de1a3f0d513f668713446e993b1f1c47290
Morvan Antoine <antoine.morvan@insa-rennes.fr>
2019-10-14 17:03:45 +0200

    (Scripts) Add arguments for build preesm

--- scripts/build_preesm.sh
+++ scripts/build_preesm.sh
@@ -2,8 +2,9 @@
 
 DIR=$(cd $(dirname $0) && pwd)
 
-PREESM_CLI_BRANCH=master
-PREESM_BRANCH=develop
+PREESM_BRANCH=$1
+PREESM_CLI_BRANCH=$2
+PREESM_PATH=${DIR}/preesm/releng/org.preesm.product/target/products/org.preesm.product/linux/gtk/x86_64/
 
 PREESM_BUILD=1
 echo " -- Fetch repos"
@@ -14,17 +15,17 @@ else
   echo "   * Preesm CLI present - update"
   (cd ${DIR}/preesm-cli && git clean -xdf && git reset --hard && git checkout ${PREESM_CLI_BRANCH} && git fetch --all -p && git pull --rebase) &> ${DIR}/preesm_cli_update.log
 fi
-if [ ! -d preesm ]; then
-  echo "   * Clone Preesm CLI"
+if [ ! -d ${DIR}/preesm ]; then
+  echo "   * Clone Preesm"
   (cd ${DIR}/ && git clone https://github.com/preesm/preesm.git -b ${PREESM_BRANCH} &> ${DIR}/preesm_clone.log)
 else
   echo "   * Preesm present"
-  CURRENT_PREESM_BRANCH=$(cd preesm && git branch --no-color | colrm 1 2)
+  CURRENT_PREESM_BRANCH=$(cd ${DIR}/preesm && git branch --no-color | colrm 1 2)
   if [ "${CURRENT_PREESM_BRANCH}" != "${PREESM_BRANCH}" ]; then
     echo "    - clean, switch branch, pull"
     (cd ${DIR}/preesm && git clean -xdf && git reset --hard && git checkout ${PREESM_BRANCH} && git fetch --all -p && git pull --rebase)  &> ${DIR}/preesm_update.log
   else 
-    NB_DIFF=$(cd preesm && git diff --stat ${PREESM_BRANCH}..origin/${PREESM_BRANCH} | wc -l)
+    NB_DIFF=$(cd ${DIR}/preesm && git diff --stat ${PREESM_BRANCH}..origin/${PREESM_BRANCH} | wc -l)
     if [ $NB_DIFF == 0 ]; then
       echo "    - already up to date"
       PREESM_BUILD=0
@@ -37,7 +38,7 @@ fi
 
 
 echo " -- Build Preesm"
-if [ ! -x ${DIR}/preesm/releng/org.preesm.product/target/products/org.preesm.product/linux/gtk/x86_64/eclipse ]; then
+if [ ! -x ${PREESM_PATH}/eclipse ]; then
   PREESM_BUILD=1
 fi
 
@@ -54,4 +55,3 @@ if [ $PREESM_BUILD != 0 ]; then
 else
   echo "   * Skip: no git update and product present"
 fi
-
