6475aaf64f917493ceaaf4a11b1793219b5dc1c9
Morvan Antoine <antoine.morvan@insa-rennes.fr>
2019-10-14 17:40:26 +0200

    (Scripts) Add stabilization

---
+++ org.ietr.preesm.stabilization/preesm_integration_config.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -eu
+
+export PROJ_NAME="Stabilization"
+
+
+export SCENARIOS="1coreX86.scenario 4coresX86.scenario 12coresX86.scenario"
+export WORKFLOWS="Codegen_basic_memory.workflow Codegen.workflow"
+
+export REF_SCENARIO=1coreX86.scenario
+export REF_WORKFLOW=Codegen_basic_memory.workflow
+
+function preesm_project_fetch_data () {
+  TTF_FILE=Code/dat/DejaVuSans.ttf
+  YUV_FILE=Code/dat/JaguarAndCroco360x202-375f.yuv
+  [ ! -f $TTF_FILE ] && wget -O Code/dat/DejaVuSans.ttf https://preesm.github.io/assets/downloads/DejaVuSans.ttf
+  [ ! -f $YUV_FILE ] && wget -O Code/dat/JaguarAndCroco720x404-375f.mp4 http://i.imgur.com/WMqwno7.mp4 && (cd Code/dat && ffmpeg -i JaguarAndCroco720x404-375f.mp4 -vf scale=360:202 -c:v rawvideo -pix_fmt yuv420p JaguarAndCroco360x202-375f.yuv)
+  return 0
+}
+
+function preesm_project_build () {
+  (mkdir -p Code/bin && cd Code/bin && rm -rf * && cmake -D CMAKE_C_FLAGS="-DPREESM_VERBOSE -DPREESM_LOOP_SIZE=300 " .. && make -j4)
+  return $?
+}
+
+function preesm_project_exec () {
+  (cd Code/ && xvfb-run -a --server-args="-ac -screen 0 1280x1024x16" ./bin/Release/stabilization)
+  return $?
+}
