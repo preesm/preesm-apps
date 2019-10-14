4291a0edbdd1785ad1f5856348bd195603fd07e1
Morvan Antoine <antoine.morvan@insa-rennes.fr>
2019-10-14 16:45:29 +0200

    (Scripts) Delete unused scripts

--- stereo/org.ietr.preesm.stereo/Code/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DIR=$(cd $(dirname $0) && pwd)
-
-rm -rf ${DIR}/bin
-mkdir -p ${DIR}/bin
-(cd ${DIR}/bin && cmake -D CMAKE_C_FLAGS="-DPREESM_VERBOSE -DPREESM_LOOP_SIZE=5 " .. && make VERBOSE=1)
