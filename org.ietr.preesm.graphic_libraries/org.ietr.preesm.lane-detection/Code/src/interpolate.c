5425dadeda9fb032092901cc531aa312a370e2b5
Florian Arrestier <florian.arrestier@insa-rennes.fr>
2020-02-17 14:22:11 +0100

    (APPS) fixed CMakeLists with zhijin name.

--- org.ietr.preesm.graphic_libraries/org.ietr.preesm.lane-detection/Code/src/interpolate.c
+++ org.ietr.preesm.graphic_libraries/org.ietr.preesm.lane-detection/Code/src/interpolate.c
@@ -1,15 +1,12 @@
 #include "interpolate.h"
 
-void interpolate(int width, int height, unsigned char *input,
-		unsigned char *output) {
-	int i, j, k;
-	int res;
-
-	for (j = 0; j < height; j++) {
+void interpolate(int width, int height, unsigned char *input, unsigned char *output) {
+	int k = 0;
+	for (int j = 0; j < height; j++) {
 		output[k] = input[k + 1];
 		k++;
-		for (i = 1; i < width - 1; i++) {
-			res = (((int) input[k - 1]) + ((int) input[k + 1])) / 2;
+		for (int i = 1; i < width - 1; i++) {
+			int res = (((int) input[k - 1]) + ((int) input[k + 1])) / 2;
 			output[k++] = (unsigned char) res;
 		}
 		output[k] = input[k - 1];
--- org.ietr.preesm.graphic_libraries/org.ietr.preesm.lane-detection/Code/src/ipm.c
+++ org.ietr.preesm.graphic_libraries/org.ietr.preesm.lane-detection/Code/src/ipm.c
@@ -3,20 +3,17 @@
 
 void ipm(int width, int height, int x11, int x12, int x21, int x22, int y1,
 		int y2, unsigned char *input, unsigned char *output) {
-	int i, j, k;
 	int frame_w1 = x12 - x11;
 	int frame_w2 = x22 - x21;
-	int frame_w;
 	int frame_h = y2 - y1;
-	int x_set;
 
-	k = 0;
-	for (j = 0; j < height; j++) {
-		x_set = x11 + (j * (x21 - x11)) / height;
-		x_set += (y1 + (j * frame_h) / height) * width;
-		frame_w = frame_w1 + (j * (frame_w2 - frame_w1)) / height;
-
-		for (i = 0; i < width; i++) {
+	int k = 0;
+	memset(output, 0, (size_t) (width * height));
+	for (int j = 0; j < height; j++) {
+		int x_set = (x11 + (j * (x21 - x11)) / height) +
+		            (y1 + (j * frame_h) / height) * width);
+		int frame_w = frame_w1 + (j * (frame_w2 - frame_w1)) / height;
+		for (int i = 0; i < width; i++) {
 			output[k++] = input[x_set + (i * frame_w) / width];
 		}
 	}
@@ -24,20 +21,16 @@ void ipm(int width, int height, int x11, int x12, int x21, int x22, int y1,
 
 void ipmReverse(int width, int height, int x11, int x12, int x21, int x22, int y1,
 		int y2, unsigned char *input, unsigned char *output) {
-	int i, j, k;
 	int frame_w1 = x12 - x11;
 	int frame_w2 = x22 - x21;
-	int frame_w;
 	int frame_h = y2 - y1;
-	int x_set;
-
-	k = 0;
-	for (j = 0; j < height; j++) {
-		x_set = x11 + (j * (x21 - x11)) / height;
-		x_set += (y1 + (j * frame_h) / height) * width;
-		frame_w = frame_w1 + (j * (frame_w2 - frame_w1)) / height;
-
-		for (i = 0; i < width; i++) {
+	int k = 0;
+	memset(output, 0, (size_t) (width * height));
+	for (int j = 0; j < height; ++j) {
+		int x_set = (x11 + (j * (x21 - x11)) / height) +
+		            (y1 + (j * frame_h) / height) * width);
+		int frame_w = frame_w1 + (j * (frame_w2 - frame_w1)) / height;
+		for (int i = 0; i < width; ++i) {
 			output[x_set + (i * frame_w) / width] = input[k++];
 		}
 	}
